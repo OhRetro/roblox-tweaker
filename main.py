ff5c096c4c3d05d20dcbecd510f3edfa4a0d658e
OhRetro <gabrielnacazone@hotmail.com>
2022-04-02 20:16:36 -0300

    v2.1 dev - change 1

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 #Roblox Tweaker
-_version = "2.0"
+_version_info = [[2,1,0], "Dev"]
+_version = f"{_version_info[0][0]}.{_version_info[0][1]}.{_version_info[0][2]}, {_version_info[1]}"
 
 #Imports
 try:
     from oreto_utils import Terminal
     from oreto_utils import File
-    
+
 except ImportError as missing_package:
-    missing_package = str(missing_package).replace("No module named ", "")
-    print(f"{missing_package} is missing.\nPlease install {missing_package}.\n")
+    print(missing_package)
     exit(0)
 
 finally:
     from os import environ as os_environ
-    from os import name as os_name
     from tkinter import Tk, filedialog
     from shutil import rmtree as sh_rmtree
 
@@ -22,8 +21,7 @@ class RobloxTweaker():
         Terminal.title(f"Roblox Tweaker v{_version}")
         Terminal.clear()
                 
-        self.dectect_os()
-        
+        self._roblox_versions_path = os_environ["LocalAppData"]+"/Roblox/Versions"
         _platform_content = "/PlatformContent/pc"
         self._textures_folders_path = f"{_platform_content}/textures"
         
@@ -41,10 +39,9 @@ class RobloxTweaker():
             self.menu()
 
     def menu(self):
-        print(f"Roblox Tweaker v{_version} | OS: {self.user_os}\n")
+        print(f"Roblox Tweaker v{_version}")
         print("What do you want to do?\n")
         print("[D]elete Textures\n[U]pdate Roblox Version Folder Path\n[E]xit\n")
-        if self.user_os == "Linux": print("Update [G]rapejuice Prefix\n")
         print(f"Current Roblox Version Folder Path:\n{self._rvf_r}\n")
         _selected_option = input(">")
         
@@ -55,11 +52,10 @@ class RobloxTweaker():
         elif _selected_option in ["E", "e", "3"]:
             Terminal.clear()
             exit(0)
-        elif self.user_os == "Linux" and _selected_option in ["G", "g", 4]:
-            self.update_gjp()    
         else:
             Terminal.clear()
 
+    #Delete Textures
     def delete_textures(self):
         if not self.roblox_version_file.exists():
             Terminal.clear()
@@ -77,35 +73,12 @@ class RobloxTweaker():
             Terminal.clear()
             print("[There is no textures available to delete.]\n")
 
+    #Update Roblox Version Folder Path
     def update_rvf_path(self):
         _updated = self.write_rversion_path_file()
         Terminal.clear()
         if _updated: print("[Updated]\n")
         else: print("[Operation Canceled]\n")
-
-    def update_gjp(self):
-        _updated = self.write_prefix_file()
-        Terminal.clear()
-        if _updated: print("[Updated]\n")
-        else: print("[Operation Canceled]\n")
-
-    def dectect_os(self):
-        if os_name == "nt":
-            self.user_os = "Windows"
-            self._roblox_versions_path = os_environ["LocalAppData"]+"/Roblox/Versions"
-            
-        elif os_name == "posix":
-            self.user_os = "Linux"
-            self._grapejuice_prefixes_path = os_environ["HOME"]+"/.local/share/grapejuice/prefixes"
-            
-            self.grapejuice_prefix_file = File("grapejuice-prefix", "")
-            
-            if not self.grapejuice_prefix_file.exists():
-                self.write_prefix_file()
-            if self.grapejuice_prefix_file.exists:
-                self._gjp_r = self.grapejuice_prefix_file.read()
-
-            self._roblox_versions_path = f"{self._gjp_r}/drive_c/Program Files (x86)/Roblox/Versions"
 
     def select_directory(self, title, initialdir):
         dialog = Tk()
@@ -119,14 +92,6 @@ class RobloxTweaker():
             return False
 
         self.roblox_version_file.write(self.roblox_version_path)
-        return True
-
-    def write_prefix_file(self):
-        self._grapejuice_prefix_path = self.select_directory("Select a prefix", self._grapejuice_prefixes_path)
-        if type(self._grapejuice_prefix_path) == tuple:
-            return False
-
-        self.grapejuice_prefix_file.write(self._grapejuice_prefix_path)
         return True
 
 
