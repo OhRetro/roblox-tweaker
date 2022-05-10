faddbc79bd34e43b1b4983f340b6e4f85f6297d0
OhRetro <gabrielnacazone@hotmail.com>
2022-05-10 18:58:41 -0300

    v2.2

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Roblox Tweaker
 ## [Download](https://github.com/OhRetro/Roblox-Tweaker/archive/refs/heads/stable-stage.zip).
-## [Download Roblox Texture Remover Legacy](https://github.com/OhRetro/Roblox-Tweaker/releases/tag/Legacy).
-An easier automatic way to remove the textures from Roblox. <br/>
-It is recommended using [Roblox FPS Unlocker](https://github.com/axstin/rbxfpsunlocker/releases) made by [axstin](https://github.com/axstin). <br/>
 
-Version: v2.1
+An one of the automatic ways to tweak Roblox. <br/>
+I recommended using [Roblox FPS Unlocker](https://github.com/axstin/rbxfpsunlocker/releases) made by [axstin](https://github.com/axstin). <br/>
+
+Version: v2.2
 
 **Note: This is not a FPS booster;**
 
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 #Roblox Tweaker
-_version = ["2.1", "Stable"]
+_version = ["2.2", "Dev-Unstable"]
 
 #Imports
 try:
-    from oreto_utils import Terminal
-    from oreto_utils import File
+    from oreto_utils import Terminal, File, Folder, Folders
 
 except ImportError as missing_package:
     print(missing_package)
@@ -12,9 +11,6 @@ except ImportError as missing_package:
 
 finally:
     from os import environ as os_environ, name as os_name
-    from tkinter import Tk, filedialog
-    from shutil import rmtree as sh_rmtree
-    from contextlib import suppress as ctxl_suppress
 
 if os_name != "nt":
     print("This program was intended to be run on Windows, Exiting...")
@@ -30,89 +26,79 @@ class RobloxTweaker():
                 
         self._roblox_versions_path = os_environ["LocalAppData"]+"/Roblox/Versions"
         self._textures_folders_path = "/PlatformContent/pc/textures"
+                
+        self._exception_folders = ["sky", "brdfLUT.dds", "studs.dds", "wangIndex.dds"]
         
-        self._textures_folders_list = ["aluminum", "brick", "cobblestone", "concrete", "diamondplate", "fabric", "glass", "granite", "grass",
-                                       "ice", "marble", "metal", "pebble", "plastic", "rust", "sand", "slate", "water", "wood", "woodplanks"]
-
-        self.roblox_version_file = File(file_name="roblox-version-folder", file_ext="")
+        self.path_file = File("path", ".txt")
+        self.path_dir = Folder("", self._roblox_versions_path)
         
-        if not self.roblox_version_file.exists():
-            self.write_file()
+        if not self.path_file.exists():
+            print("[Path File Not Found.]")
+            self.writepath()
+            Terminal.clear()
             
-        if self.roblox_version_file.exists:
-            self._rvf_r = self.roblox_version_file.read()
+        if self.path_file.exists:
+            self.path = self.path_file.read()
+            self.path_folder = Folder("", self.path)
         
         while self.running:
-            self.menu()
-
-    def menu(self):
-        print(f"Roblox Tweaker v{_version[0]} {_version[1]}")
-        print("What do you want to do?\n")
-        print("[D]elete Textures\n[U]pdate Roblox Version Folder Path\n[E]xit\n")
-        print(f"Current Roblox Version Folder Path:\n\"{self._rvf_r}\"\nType: {self.get_type()}\n")
-        _selected_option = input(">")
+            print(f"Roblox Tweaker v{_version[0]} {_version[1]}")
+            print("What do you want to do?\n")
+            print("[D]elete Textures\n[U]pdate Roblox Version Folder Path\n[E]xit\n")
+            print(f"Current Roblox Version Folder Path:\n\"{self.path}\"\nType: {self.gettype()}\nOutdated Path: {self.outdated()}\n")
+            _selected_option = input(">")
+            
+            if _selected_option in ["D", "d", "1"]:
+                self.deletetextures()    
+                
+            elif _selected_option in ["U", "u", "2"]:
+                self.writepath()
+                
+            elif _selected_option in ["E", "e", "3"]:
+                Terminal.clear()
+                self.exit_code = 0
+                self.running = False
+                
+            else:
+                Terminal.clear()
         
-        if _selected_option in ["D", "d", "1"]:
-            self.delete_textures()    
-            
-        elif _selected_option in ["U", "u", "2"]:
-            self.update_path()
-            
-        elif _selected_option in ["E", "e", "3"]:
-            Terminal.clear()
-            self.exit_code = 0
-            self.running = False
-            
-        else:
-            Terminal.clear()
-
     #Delete Textures
-    def delete_textures(self):
-        for _texture_folder in self._textures_folders_list:
-            with ctxl_suppress(FileNotFoundError):
-                sh_rmtree(f"{self._rvf_r}/{self._textures_folders_path}/{_texture_folder}")
+    def deletetextures(self):
+        textures_path = Folder(self._textures_folders_path, self.path)
+        textures_path.deletecontents(self._exception_folders)
                 
         Terminal.clear()
         print("[Textures Deleted.]\n")
         
-    #Update Roblox Version Folder Path
-    def update_path(self):
-        _updated = self.write_file()
-        self._rvf_r = self.roblox_version_file.read()
-        Terminal.clear()
-        
-        if _updated:
-            print("[Updated]\n")
-        else:
-            print("[Operation Canceled]\n")
-
-    #Select Directory
-    def select_directory(self, title, initialdir):
-        dialog = Tk()
-        dialog.withdraw()
-
-        return filedialog.askdirectory(title=title, initialdir=initialdir, parent=dialog)
-
     #Write File
-    def write_file(self):
-        self.roblox_version_path = self.select_directory("Select a Roblox Version Folder.", self._roblox_versions_path)
+    def writepath(self):
+        while True:
+            roblox_version_path = self.path_dir.select("", title="Select Roblox Version Folder", initialdir=self._roblox_versions_path,)
+                
+            if roblox_version_path:
+                break
+        self.path_file.write(roblox_version_path)
+        Terminal.clear()
+        print("[Path File Written.]\n")
         
-        if self.roblox_version_path == "":
-            return False
-
-        self.roblox_version_file.write(self.roblox_version_path)
-        return True
-
     #Get which Roblox Type is currently selected
-    def get_type(self):
-        rop = File(file_name="RobloxPlayerBeta", file_ext=".exe", file_path=f"{self._rvf_r}")
-        ros = File(file_name="RobloxStudioBeta", file_ext=".exe", file_path=f"{self._rvf_r}")
+    def gettype(self):
+        roblox_exes = ["RobloxPlayerBeta.exe", "RobloxStudioBeta.exe"]
+        roblox_version_path = Folder("", self.path).list()
 
-        if rop.exists():
-            return "Roblox Player"
-        elif ros.exists():
-            return "Roblox Studio"
+        for exe in roblox_exes:
+            if exe in roblox_version_path and exe == "RobloxPlayerBeta.exe":
+                return "Roblox Player"
+            elif exe in roblox_version_path and exe == "RobloxStudioBeta.exe":
+                return "Roblox Studio"
+            else:
+                return "Unknown"
 
+    #Check if Roblox Version Path is empty to declare as outdated or not
+    def outdated(self):
+        path_list = self.path_folder.list()
+        return path_list == []
+        
 if __name__ == "__main__":
-    _rt = RobloxTweaker()
-    exit(_rt.exit_code)
+    rt = RobloxTweaker()
+    exit(rt.exit_code)
