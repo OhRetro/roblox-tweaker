af07fd1505c96731ef8f633876125d26a5f79814
OhRetro <gabrielnacazone@hotmail.com>
2022-08-05 19:57:54 -0300

    yes

--- Workings/MainVariables.cs
+++ Workings/MainVariables.cs
@@ -5,7 +5,7 @@
         public static readonly string ROBLOX_VERSIONS_DIR = Environment.GetEnvironmentVariable("LOCALAPPDATA") + "\\Roblox\\Versions";
         public static string ROBLOX_VERSION_DIR = "Not Set";
         public static string ROBLOX_VERSION_DIR_TYPE = "Not Set";
-        public const string SETTINGS_FILE = ".\\RobloxVersionDir.txt";
+        public const string SETTINGS_FILE = ".\\settings.txt";
 
         public static string ROBLOX_TEXTURE_DIR = "Not Set";
         public const string PATH_TO_TEXTURES_DIR = "\\PlatformContent\\pc\\textures";
